419c6c9025ec4f6da52f02805df5625f1efeb005
erug-the-creator <rugenstein.emil@gmail.com>
2020-12-13 16:02:42 +0100

    +

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id='app'>
+    <b-navbar toggleable='lg' type='dark' variant='info' class="mb-4">
+      <b-navbar-brand href='#'>SW</b-navbar-brand>
+
+      <b-navbar-toggle target='nav-collapse'></b-navbar-toggle>
+
+      <b-collapse id='nav-collapse' is-nav>
+        <b-navbar-nav>
+          <b-nav-item href='#'><router-link to='/'>Home</router-link></b-nav-item>
+          <b-nav-item href='#'><router-link to='/about'>About</router-link></b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
+<style lang='scss'>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='home-view'>
+    <h1 class='mb-4'>Spende Wärme</h1>
+    <b-container
+      class='mx-auto p-4 text-center home-card'
+    >
+    <br>
+      <h2>Was machst du wenn dir kalt ist?
+      </h2>
+      <p>
+        Ins Warme gehen oder dich dicker anziehen?
+        Für viele Menschen ist das leider keine Option im Winter.
+      </p>
+      <br>
+      <h2>Wir haben entschieden zu helfen!</h2>
+      <p>Mit unserer Aktion wolllen wir auf das alljährliche Problem aufmerksam machen. <br>
+        Gerade in Flüchltlingscamps in Syrien, Libanon oder dem Irak bedeutet Winter oft Angst und manchmal sogar Tod. Mit einer Spende an die UNO-Flüchtlingshilfe können wir das zusammen ändern.</p>
+
+      <b-button href='#' variant='primary'>Jetzt Spenden</b-button>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style>
+  .home-card{
+    background-color:lightblue;
+    border-radius: 9px;
+  }
+</style>
